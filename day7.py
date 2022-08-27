ba30e4cc94c4ecc4d30461352cbbc116f6886f0b
Stefan Bramer <s.bramer@gmx.com>
2022-08-27 07:10:40 +0100

    day7 - hangman

--- day7.py
+++ day7.py
@@ -1,98 +1,58 @@
 import random
 
-word_list = ["aardvark", "baboon", "camel"]
+from replit import clear
 
-stages = [
-    """
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-""",
-    """
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-""",
-    """
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-""",
-    """
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========""",
-    """
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-""",
-    """
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-""",
-    """
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-""",
-]
+import day7_art
+import day7_words
 
 # variables
+word_list = day7_words.word_list
+stages = day7_art.stages
+logo = day7_art.logo
 chosen_word = random.choice(word_list)
 solution = []
+guesses = []
 game_over = False
 lifes = 6
 
-print(chosen_word)
+print(logo)
 
+# prepare empty list and print it
 for x in range(len(chosen_word)):
     solution.append("_")
-print(solution)
+print(f"{' '.join(solution)}")
 
 # game loop
 while not game_over:
+    hits = 0
     guess = input("Please guess a letter: ").lower()
+    clear()
     if guess not in chosen_word:
         lifes -= 1
+        print(f"Sorry, '{guess}' is not in the word. You lose a life.")
+        if lifes == 0:
+            game_over = True
+            print(
+                f"GAME OVER. You've lost all your lifes! The word was '{chosen_word}'."
+            )
     else:
         for x, letter in enumerate(chosen_word):
             if letter == guess:
                 solution[x] = " " + guess + " "
-    print(solution)
+                hits += 1
+        print(f"YES, '{guess}' is in the word {hits} time(s).")
+
+    # flag if user has already guessed this letter
+    if guess in guesses:
+        print(f"(You have guessed '{guess}' already.)")
+    else:
+        guesses.append(guess)
+
+    # print current state of solution and lifes (hangman)
+    print(f"{' '.join(solution)}")
     print(stages[lifes])
-    if lifes == 0:
-        print("You lose!")
-        break
+
+    # win conditon
     if "_" not in solution:
         game_over = True
         print("You WIN!!!")
