5a8bee067bb242490d6c3a7d0075a8384cdf4d65
Stefan Bramer <s.bramer@gmx.com>
2022-09-01 13:43:09 +0100

    final commit

--- day17_quiz/main.py
+++ day17_quiz/main.py
@@ -1,5 +1,6 @@
 from data import question_data
 from question_model import MyQuestion
+from quiz_brain import QuizBrain
 
 question_bank = []
 
@@ -9,4 +10,10 @@ for question in question_data:
     new_question = MyQuestion(question_text, question_answer)
     question_bank.append(new_question)
 
-print(question_bank[0].text)
+quiz = QuizBrain(question_bank)
+while quiz.still_has_questions():
+    quiz.next_question()
+    print(f"Your score is {quiz.score}/{quiz.question_number}")
+    print()
+print("You finished the quiz.")
+print(f"Your final score is {quiz.score}/{quiz.question_number}")
--- day17_quiz/quiz_brain.py
+++ day17_quiz/quiz_brain.py
@@ -1,2 +1,30 @@
+class QuizBrain:
+    """docstring"""
+    def __init__(self, q_list) -> None:
+        self.question_number = 0
+        self.question_list = q_list
+        self.score = 0
 
-        
+    def still_has_questions(self):
+        """ doc """
+        #print(f"Question {self.question_number} of {len(self.question_list)} total questions.")
+        return self.question_number < len(self.question_list)
+
+    def next_question(self):
+        """ doc """
+        current_question = self.question_list[self.question_number]
+        self.question_number += 1
+        user_answer = input(
+            f"Q{self.question_number}: {current_question.text} (True/False): "
+        )
+        self.check_answer(user_answer, current_question.answer)
+
+    def check_answer(self, user_a, correct_a):
+        """ doc """
+        if user_a.lower() == correct_a.lower():
+            print("Yay, this is correct.")
+            self.score += 1
+        else:
+            print("Sorry, wrong. Think again..")
+        print(f"The correct answer was {correct_a}.")
+    
