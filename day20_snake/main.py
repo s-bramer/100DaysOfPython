80c55d35902a327047f316bf089f976a37a2e70d
Stefan Bramer <s.bramer@gmx.com>
2022-09-06 17:09:23 +0100

    initial commit

---
+++ day20_snake/main.py
@@ -0,0 +1,19 @@
+from turtle import Screen, Turtle
+from snake import Snake
+
+screen = Screen()
+screen.setup(width=600,height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+game_over = False
+
+while not game_over:
+    screen.update()
+    
+
+
+
+
+screen.exitonclick()
