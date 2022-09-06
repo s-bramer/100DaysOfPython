3663f8d6a5d59369395b99c36e16703786932345
Stefan Bramer <s.bramer@gmx.com>
2022-09-06 20:28:00 +0100

    movement complete

--- day20_snake/main.py
+++ day20_snake/main.py
@@ -1,5 +1,6 @@
 from turtle import Screen, Turtle
 from snake import Snake
+import time
 
 screen = Screen()
 screen.setup(width=600,height=600)
@@ -8,10 +9,20 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 game_over = False
+snake = Snake()
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 while not game_over:
+    snake.move()
     screen.update()
-    
+    time.sleep(0.1)
+    if snake.segments[0].xcor() > 300 or snake.segments[0].xcor() < -300:
+        screen.textinput("GAME OVER", "END")
+        exit()
 
 
 
--- day20_snake/snake.py
+++ day20_snake/snake.py
@@ -1,7 +1,39 @@
+from turtle import Turtle
+STARTING_POSITION = [(0,0),(-20,0),(-40,0)]
+
 class Snake:
     """This is the snake class"""
     def __init__(self) -> None:
-        self.length = 3
-        draw_snake(self.length)
-    def move():
-        
+        #self.length = 3
+        self.pixel = 20
+        self.segments = []
+        self.create_snake()
+
+    def move(self):
+        """Moving the snake about"""
+        for x in range(len(self.segments)-1,0,-1):
+            new_x = self.segments[x-1].xcor()
+            new_y = self.segments[x-1].ycor()
+            self.segments[x].goto(new_x,new_y)
+        self.segments[0].forward(self.pixel)    
+
+    def create_snake(self):
+        """Inital step to create starting snake"""
+        for segment in STARTING_POSITION:
+            new_segment = Turtle("square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(segment)
+            self.segments.append(new_segment)
+    def up(self):
+        """Moves the snake up"""
+        self.segments[0].setheading(90)    
+    def down(self):
+        """Moves the snake down"""
+        self.segments[0].setheading(270)
+    def left(self):
+        """Moves the snake left"""
+        self.segments[0].setheading(180)   
+    def right(self):
+        """Moves the snake right"""
+        self.segments[0].setheading(0)
