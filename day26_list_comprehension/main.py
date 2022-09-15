26aefff454be47add923a207aa1aeefd5df30747
Stefan Bramer <s.bramer@gmx.com>
2022-09-15 14:23:18 +0100

    error handling added

--- day26_list_comprehension/main.py
+++ day26_list_comprehension/main.py
@@ -10,10 +10,18 @@ data = pd.read_csv("./day26_list_comprehension/nato_phonetic_alphabet.csv")
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
 phonetic_dict = {row.letter: row.code for (index,row) in data.iterrows()}
-print (phonetic_dict)
+#print (phonetic_dict)
 
 #2. Create a list of the phonetic code words from a word that the user inputs.
-word = input("Input a word: ")
-output = [phonetic_dict[letter.upper()] for letter in word]
-#output = [phonetic_dict.values for letter in word if letter == phonetic_dict.keys ]
-print(output)
+#3. Use exceptions to catch wrong user entry and recursively recall function
+def generate_phonetics():
+    word = input("Input a word: ")
+    try:
+        output = [phonetic_dict[letter.upper()] for letter in word]
+    except KeyError:
+        print("Only letters in the alphabet please - try again.")
+        generate_phonetics()
+    else:
+        print(output)
+
+generate_phonetics()
