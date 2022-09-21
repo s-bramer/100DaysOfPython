bcd1558b60d4386510787944295b0ec5ee2c8e96
Stefan Bramer <s.bramer@gmx.com>
2022-09-21 08:47:36 +0100

    authentification sorted

--- day32_mail/main.py
+++ day32_mail/main.py
@@ -1,16 +1,23 @@
 """
 sending emails via python
 currently does not work because of policy restrictions
+need to use 2 step varification and app password
 """
 
 import smtplib
 
+SENDER_EMAIL = "pickled.sprout.bay@gmail.com"
+PASSWORD = ""
+RECEIVER_EMAIL = "s.schultchen@gmx.com"
+TEXT = "Hello, I am writing an email using only python"
+SUBJECT = "Python Email"
+MESSAGE = f"Subject: {SUBJECT}\n\n{TEXT}"
 
-my_email = "pickled.sprout.bay@gmail.com"
-password = "Googlebox1"
-connection = smtplib.SMTP("smtp.gmail.com") 
-connection.starttls() #make connection secure
-connection.login(user=my_email, password=password)
-connection.sendmail(from_addr=my_email, to_addrs="s.schultchen@gmx.com", msg="Hello, I am writing an email using only python")
-connection.close
+connection = smtplib.SMTP("smtp.gmail.com")
+connection.ehlo()
+connection.starttls() #make connection secure (Transport Layer Security)
+connection.ehlo()
+connection.login(user=SENDER_EMAIL, password=PASSWORD)
 
+connection.sendmail(from_addr=SENDER_EMAIL, to_addrs=RECEIVER_EMAIL, msg=MESSAGE)
+connection.quit()
