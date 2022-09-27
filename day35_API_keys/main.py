1fd55b2867d9428bbc8d1c804c6f3c8210ff1580
Stefan Bramer <s.bramer@gmx.com>
2022-09-27 14:49:21 +0100

    env variable added

--- day35_API_keys/main.py
+++ day35_API_keys/main.py
@@ -1,17 +1,21 @@
+import os
 import requests
-
-OWM_endpoint = "https://api.openweathermap.org/data/3.0/onecall"
-API_key = "96a1e17cd654862d44b04f13936aab3c"
 
 MY_LAT = 51.402560 # Your latitude
 MY_LONG = -3.484190 # Your longitude
+OWM_endpoint = "https://api.openweathermap.org/data/2.5/weather"
+api_key = os.getenv("OWM_API_KEY")
+print(api_key)
 
 parameters = {
-"lat": MY_LAT,
-"lon": MY_LONG,
-"appid": API_key,
+    "lat": MY_LAT,
+    "lon": MY_LONG,
+    "appid": api_key,
 }
 
 response = requests.get(OWM_endpoint, params=parameters)
 response.raise_for_status()
-print(response.json())
+data = response.json()
+
+print(data["weather"][0]["description"])
+#print(os.getenv("PROJECT_DIR"))
