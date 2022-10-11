bf33ef4c59ee01aaa45ccf0d858d0079031e89f9
Stefan Bramer <s.bramer@gmx.com>
2022-10-11 20:15:00 +0100

    templates and static files

--- day54_flask/main.py
+++ day54_flask/main.py
@@ -1,8 +1,38 @@
 from flask import Flask
-app = Flask(__name__)
 
-@app.route('/') # show me the home page (the root)
-def hello_world():
+def make_bold(function):
+    #print(f"Output: {function()}")
+    def bold():
+        return f"<b> {function()} </b>"
+    return bold
+def make_underline(function):
+    #print(f"Output: {function()}")
+    def underline():
+        return f"<u> {function()} </u>"
+    return underline
+def make_italics(function):
+    #print(f"Output: {function()}")
+    def italics():
+        return f"<i> {function()} </i>"
+    return italics
+app = Flask(__name__) # flask needs to check that the current file is the correct one 
+
+@app.route('/')
+@make_bold
+@make_underline
+@make_italics
+def hello_world(): 
     return 'Hello, Worldy!'
+
+@app.route('/bye')
+def say_bye():
+    return 'Bye Bye!'
+
+#adding variables with <variable>
+@app.route('/username/<name>/<int:number>')
+def greet(name,number):
+    return f"Hello {name}, you are {number} years old!"
+
+#e.g. enter http://127.0.0.1:5000/username/stefan/2
 
 app.run()    
