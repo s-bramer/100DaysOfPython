a2fe896d0dc2d0ddfd4874fcb6b67a3f70c03746
Stefan Bramer <s.bramer@gmx.com>
2022-10-11 15:57:12 +0100

    advanced decorators

--- day54_flask/decorators.py
+++ day54_flask/decorators.py
@@ -23,5 +23,22 @@ def slow_function():
     for i in range(100000000):
         i * i
 
-fast_function()
-slow_function()
+#fast_function()
+#slow_function()
+
+#advanced decorators, decorate function with arguments
+#log the name of the function that was called, the arguments and the returned output
+def logging_decorator(function):
+    def wrapper(*args,**kwargs):
+        function_name = function.__name__
+        output = function(args[0],args[1],args[2])
+        print(f"You called {function_name}{args}")
+        print(f"It returned: {output}")
+    return wrapper
+
+
+@logging_decorator
+def a_function(x,y,z):
+    return x*y*z
+
+a_function(1,2,3)
