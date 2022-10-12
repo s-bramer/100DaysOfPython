9cd4bb772405d8d8e3ad413b9ac57df8523796f5
Stefan Bramer <s.bramer@gmx.com>
2022-10-12 20:00:59 +0100

    flask with jinja templates

--- day54_flask/server.py
+++ day54_flask/server.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template
+from datetime import date
+import requests
 
+
+current_year = date.today().year
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("portfolio.html")
+    return render_template("portfolio.html", year=current_year)
 
+@app.route('/blog')
+def blog():
+    blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
+    response = requests.get(blog_url)
+    all_posts = response.json()
+    return render_template("blog.html", posts=all_posts)
 if __name__ == "__main__":
     app.run()
