d1481c70758654feb1b433f68d6ebb53d874f0ee
Stefan Bramer <s.bramer@gmx.com>
2022-10-16 10:55:03 +0100

    empty blog template with flask and jinja

--- day57_blog/main.py
+++ day57_blog/main.py
@@ -1,23 +1,18 @@
 from flask import Flask, render_template
-import requests
-
 
 app = Flask(__name__)
-blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
-response = requests.get(blog_url)
-all_posts = response.json()
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template("index.html", posts=all_posts)
+    return render_template("index.html")
 
-@app.route("/post/<int:index>")
-def show_post(index):
-    requested_post = None
-    for blog_post in all_posts:
-        if blog_post["id"] == index:
-            requested_post = blog_post
-    return render_template("post.html", post=requested_post)
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
