dc0096a2951f1648c82121d1ed76c3ea9812068b
Stefan Bramer <s.bramer@gmx.com>
2022-11-03 13:02:35 +0000

    table using flask and jQuery js DataTable

--- day62_database_backend/main.py
+++ day62_database_backend/main.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Markup, request, url_for
 from flask_bootstrap import Bootstrap
+from flask_table import Table, Col, LinkCol
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
@@ -9,45 +10,34 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap(app)
 
+class SortableTable(Table):
+    id = Col('ID')
+    name = Col('Name')
+    description = Col('Description')
+    link = LinkCol(
+        'Link', 'flask_link', url_kwargs=dict(id='id'), allow_sort=False)
+    allow_sort = True
 
-class CafeForm(FlaskForm):
-    cafe = StringField('Cafe name', validators=[DataRequired()])
-    submit = SubmitField('Submit')
-
-# Exercise:
-# add: Location URL, open time, closing time, coffee rating, wifi rating, power outlet rating fields
-# make coffee/wifi/power a select element with choice of 0 to 5.
-#e.g. You could use emojis ☕️/💪/✘/🔌
-# make all fields required except submit
-# use a validator to check that the URL field has a URL entered.
-# ---------------------------------------------------------------------------
-
+    def sort_url(self, col_key, reverse=False):
+        if reverse:
+            direction = 'desc'
+        else:
+            direction = 'asc'
+        return url_for('index', sort=col_key, direction=direction)
 
 # all Flask routes below
 @app.route("/")
 def home():
     return render_template("index.html")
 
-
-@app.route('/add')
-def add_cafe():
-    form = CafeForm()
-    if form.validate_on_submit():
-        print("True")
-    # Exercise:
-    # Make the form write a new row into cafe-data.csv
-    # with   if form.validate_on_submit()
-    return render_template('add.html', form=form)
-
-
 @app.route('/cafes')
 def cafes():
-    with open('./day62_database_backend/cafe-data.csv', newline='', encoding="utf8") as csv_file:
+    with open('./day62_database_backend/events_database_no_desc.csv', newline='', encoding="utf8") as csv_file:
         csv_data = csv.reader(csv_file, delimiter=',')
         list_of_rows = []
         for row in csv_data:
-            print(row)
             list_of_rows.append(row)
+        print(list_of_rows)
     return render_template('cafes.html', cafes=list_of_rows)
 
 
---
+++ day62_database_backend/static/js/main.js
@@ -0,0 +1,13 @@
+// $(document).ready(function () {
+//   $("#dtBasicExample").DataTable();
+//   $(".dataTables_length").addClass("bs-select");
+// });
+
+// $(document).ready(function () {
+//   var table = $("#dtBasicExample").DataTable({
+//     lengthChange: false,
+//     buttons: ["copy", "excel", "pdf", "colvis"],
+//   });
+
+//   table.buttons().container().appendTo("#example_wrapper .col-md-6:eq(0)");
+// });
